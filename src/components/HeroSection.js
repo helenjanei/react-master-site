c8c465cc44e92acfe49c2eaa0941fa9666f88af2
helenjanei <helenjanei@gmail.com>
2020-12-01 12:23:58 +0000

    resolves className issues

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import { Link } from 'react-router-dom';
 
 function HeroSection() {
   return (
@@ -10,20 +11,27 @@ function HeroSection() {
       <h1>Welcome</h1>
       <p></p>
       <div className='hero-btns'>
+        <a href='https://github.com/helenjanei/react-master-site' >
         <Button
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
+          href='https://github.com/helenjanei/react-master-site'
+           target="_blank"
         >
-          GET STARTED
+          SEE THE CODE
         </Button>
+        </a>
+        <Link to='/projectvideo' >
         <Button
           className='btns'
           buttonStyle='btn--primary'
           buttonSize='btn--large'
+          href='https://google.com' 
           onClick={console.log('hey')}
         > WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
+        </Link>
       </div>
     </div>
   );
