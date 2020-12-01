4efb654304e41153a76c04a66bc23cd055d42162
helenjanei <helenjanei@gmail.com>
2020-12-01 12:18:33 +0000

    completes Travel componenets and rooting

--- src/components/pages/Travel.js
+++ src/components/pages/Travel.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './travel.css'
 import CardItem from '../CardItem'
 
@@ -7,30 +6,34 @@ export default function Travel() {
   return ( 
     <>
   <div className="travel_container">
-    <div className="logo_container">
+       <img className='travel_picture' src='images/img-12.jpg' />
     <h1  className='travel_buddy_logo'>Travel Buddy</h1>
-    </div>
-    </div>
-    <div className="travel_buddy_text">
+  </div>
+  
+  <div className="travel_buddy_text">
 
     <p>Welcome to Travel Buddy, a React app where users can explore the hidden gems of travel, submitted by fellow travellers.</p>
    <p></p>
-    <p>
-You can find the hosted GraphQL API used by the React app here: https://travel-buddy-2020.herokuapp.com/graphql
-</p>
-<p></p>
+
 <p>
-This was part of a two-week, four-person group final project at the Northcoders bootcamp. It uses the following technologies:
+This was part of a two-week, four-person final project at the <a href='https://northcoders.com/'target="_blank">Northcoders</a> bootcamp. 
+</p>
+<p>
+  
+It uses the following technologies:
 </p>
 <p></p>
-<ul>
-<li>React</li>
-<li>Leaflet</li>
-<li>React Leaflet </li>
-<li>Firebase </li>
-<li>Styled Components</li>
-<li> Axios</li>
+<ul className='techstack_ul'>
+<li className='techstack_list'>React</li>
+<li className='techstack_list'>Leaflet</li>
+<li className='techstack_list' >React Leaflet </li>
+<li className='techstack_list' >Firebase </li>
+<li className='techstack_list'>Styled Components</li>
+<li className='techstack_list'> Axios</li>
 </ul>
+    <p>
+You can find the hosted GraphQL API used by the React app <a href='https://travel-buddy-2020.herokuapp.com/graphql'target="_blank">here</a>
+</p>
     </div>
 
 
@@ -41,21 +44,23 @@ This was part of a two-week, four-person group final project at the Northcoders 
           <ul className='cards__items'>
             <CardItem
               src='images/img-9.jpg'
-              text='Use our recipe finder to create your next great meal'
-              label='Cook'
-              path='/recipefinder'
+              text='View the back end code on Git'
+              label='Back end'
+              path='/travelbuddybackend'
+             
             />
             <CardItem
-              src='images/img-9.jpg'
-              text='Use our website to find your next great travel destination'
-              label='Travel'
-              path='/travelbuddy'
+              src='images/img-4.jpg'
+              text='View the front end code on Git'
+              label='Front end'
+              path='/travelbuddyfrontend'
+             
             />
            <CardItem
               src='images/img-8.jpg'
-              text='Use our book finder to find your next great read'
-              label='Read'
-              path='/bookfinder'
+              text='View the graduate showcase for Travel Buddy'
+              label='Video'
+              path='/projectvideo'
             />
           </ul>
         </div>
---
+++ src/components/pages/TravelbuddyBE.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../../App.css';
+import { Button } from '../Button';
+import '../HeroSection.css';
+import { Link } from 'react-router-dom';
+import './travel.css';
+
+export default function TravelbuddyFE() {
+  return ( 
+  <>
+    
+       <div className='travel_hero_container_BE'>
+         
+      
+      <h1 className='travelbuddy_h1_logo'>Travel Buddy Back End</h1>
+      <p></p>
+      <div className='hero-btns'>
+        <a href='https://github.com/helenjanei/travelbuddy-backend' target="_blank">
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+          href='https://github.com/helenjanei/travelbuddy-frontend'
+           target="_blank"
+        >
+          SEE THE CODE
+        </Button>
+        </a>
+        <Link to='/projectvideo' >
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          href='https://google.com' 
+          onClick={console.log('hey')}
+        > WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+        </Link>
+      </div>
+    </div>
+      
+  </>)}
