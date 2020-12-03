3dcdc23111de0d9b2545688f3008dc98ef4c683e
helenjanei <helenjanei@gmail.com>
2020-12-03 10:01:16 +0000

    fixes video player issues

--- src/components/pages/ProjectVideo.js
+++ src/components/pages/ProjectVideo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../App.css';
+import ReactPlayer from 'react-player'
 
 export default function ProjectVideo() {
   return (
@@ -9,20 +10,14 @@ export default function ProjectVideo() {
       style={{
         position: "relative",
         paddingBottom: "56.25%" /* 16:9 */,
-        paddingTop: 25,
-        height: 0
+        paddingTop: '12vh',
+       paddingLeft: '20%',
+       
       }}
     >
-      <iframe
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%"
-        }}
-        src='https://www.youtube.com/embed/pj0Ls82cqFY&t=80s'
-        frameBorder="0"
+      <ReactPlayer 
+        url='https://www.youtube.com/embed/pj0Ls82cqFY&t=80s'
+   
       />
     </div>
 
