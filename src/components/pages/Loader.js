0f0039f12244717295f7e35831f37a1acd932c2a
helenjanei <helenjanei@gmail.com>
2020-12-04 08:10:48 +0000

    adds Loader component

---
+++ src/components/pages/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './loader.css'
+
+const Loader = () => {
+  return (
+    <div className="loader-wrapper">
+      <span className="loader">
+        <span className="loader-inner"></span>
+      </span>
+    </div>
+  );
+};
+
+export default Loader;
