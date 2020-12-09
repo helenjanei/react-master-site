d418350289fd1d5bc5cc86f7b725021bdeccfe25
helenjanei <helenjanei@gmail.com>
2020-12-09 13:02:42 +0000

    fixes some issues with flip CSS

--- src/components/pages/ProblemSolving.js
+++ src/components/pages/ProblemSolving.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactCardFlip from 'react-card-flip';
-import CardItem from '../CardItem'
+import ProblemCardItem from './ProblemCardItem'
 import '../Button.css'
 import './problem.css'
 import '../Cards.css'
 
-class ProblemCardItem extends React.Component  {
+class ProblemSolving extends React.Component  {
 
 constructor(props) {
     super(props);
@@ -26,7 +26,8 @@ constructor(props) {
   <>
   
     <div className='problem_solving'>
-      <h1>Problem Solving</h1>
+      
+       <h1> Problem Solving</h1>
     </div>
 
     <div className="cards">
@@ -37,41 +38,49 @@ constructor(props) {
             <ReactCardFlip isFlipped={this.state.isFlipped}flipSpeedBackToFront={2}
               flipSpeedFrontToBack={2} >
               <div   >
-                 <CardItem src='images/img-9.jpg' text='How to raise the English GCSE attainment of working class boys?'
+                <button className='btn btn--primary' onClick={this.handleClick}>Click here for more</button>
+                 <ProblemCardItem src='images/img-15.jpg' text='How to raise the English GCSE attainment of working class boys?'
                 label='Shakespeare' />
-                <button className='btn btn--primary' onClick={this.handleClick}>Click here for more</button>
               </div>
-              <div className='problem_card'>
-                <h4 className='cards__item__text'>How to raise the English GCSE attainment of working class boys?</h4>
-                <p className='explanation_text' >Solutions</p>
-                <li>idea one</li><li>two</li><li>three</li><li>four</li>
+              <div>
                 <button className='btn  btn--primary'  onClick={this.handleClick}>Click here for more</button>
-              </div>
+              <div className='cards__item cards__item__link'>
+                <h4 className='cards__item__text'>How to raise the English GCSE attainment of disengaged male learners?</h4>
+                <p className='explanation_text' >Solution</p>
+                
+                <p className='explanation_text'>Identified that the low national raw marks achieved by students on the Shakespeare Exam made any extra raw marks from this element raise the overall exam grade by more than those achived on any other paper. I achieved student buy-in to the strategy by using my hard data evidence to show why extra time spent by the students on Shakespeare would pay dividends. Developed in-depth resources and writing guides to improve attainment. Employed rigorous testing and improvement, using completion and immediate rewards/feedback to gain buy in from disengaged learners. Exam results showed a 9% improvement in results for the targeted group, raising their achievement above their peers nationally.</p>
+                            </div>
+                            </div>
             </ReactCardFlip>
 
 <ReactCardFlip isFlipped={this.state.isFlipped}flipSpeedBackToFront={2}
               flipSpeedFrontToBack={2} >
               <div>
-                 <CardItem src='images/img-9.jpg' text='Use my recipe finder to create your next great meal'
+                 <ProblemCardItem src='images/img-9.jpg' text='Use my recipe finder to create your next great meal'
                 label='Cook' />
-                <button className='btn btn--primary' onClick={this.handleClick}>Click here for more</button>
+               
               </div>
-              <div className='problem_card'>
-                <p className='problem_text'>Problem: How to train a dog</p>
-                <button className='btn  btn--primary'  onClick={this.handleClick}>Click here for more</button>
-              </div>
+             
+                <div className='cards__item cards__item__link'>
+             
+                <h4 className='cards__item__text'>How to raise the English GCSE attainment of disengaged male learners?</h4>
+                <p className='explanation_text' >Solution</p>
+                
+                <p className='explanation_text'>Identified that the low national raw marks achieved by students on the Shakespeare Exam made any extra raw marks from this element raise the overall exam grade by more than those achived on any other paper. I achieved student buy-in to the strategy by using my hard data evidence to show why extra time spent by the students on Shakespeare would pay dividends. Developed in-depth resources and writing guides to improve attainment. Employed rigorous testing and improvement, using completion and immediate rewards/feedback to gain buy in from disengaged learners. Exam results showed a 9% improvement in results for the targeted group, raising their achievement above their peers nationally.</p>
+                            </div>
+      
             </ReactCardFlip>
 
 <ReactCardFlip isFlipped={this.state.isFlipped}flipSpeedBackToFront={2}
               flipSpeedFrontToBack={2} >
               <div>
-                 <CardItem src='images/img-9.jpg' text='Use my recipe finder to create your next great meal'
+                 <ProblemCardItem src='images/img-9.jpg' text='Use my recipe finder to create your next great meal'
                 label='Cook' />
-                <button className='btn btn--primary' onClick={this.handleClick}>Click here for more</button>
+               
               </div>
               <div className='problem_card'>
                 <p className='problem_text'>Problem: How to train a dog</p>
-                <button className='btn  btn--primary'  onClick={this.handleClick}>Click here for more</button>
+              
               </div>
             </ReactCardFlip>
 </ul>
@@ -80,41 +89,39 @@ constructor(props) {
             <ReactCardFlip isFlipped={this.state.isFlipped}flipSpeedBackToFront={2}
               flipSpeedFrontToBack={2} >
               <div>
-                 <CardItem src='images/img-9.jpg' text='Use my recipe finder to create your next great meal'
+                 <ProblemCardItem src='images/img-9.jpg' text='Use my recipe finder to create your next great meal'
                 label='Cook' />
-                <button className='btn btn--primary' onClick={this.handleClick}>Click here for more</button>
+             
               </div>
               <div className='problem_card'>
                 <p className='problem_text'>Problem: How to train a dog</p>
-                <button className='btn  btn--primary'  onClick={this.handleClick}>Click here for more</button>
-              </div>
-            </ReactCardFlip>
-
-
-
-<ReactCardFlip isFlipped={this.state.isFlipped}flipSpeedBackToFront={2}
-              flipSpeedFrontToBack={2} >
-              <div>
-                 <CardItem src='images/img-9.jpg' text='Use my recipe finder to create your next great meal'
-                label='Cook' />
-                <button className='btn btn--primary' onClick={this.handleClick}>Click here for more</button>
-              </div>
-              <div className='problem_card'>
-                <p className='problem_text'>Problem: How to train a dog</p>
-                <button className='btn  btn--primary'  onClick={this.handleClick}>Click here for more</button>
+               
               </div>
             </ReactCardFlip>
 
 <ReactCardFlip isFlipped={this.state.isFlipped}flipSpeedBackToFront={2}
               flipSpeedFrontToBack={2} >
               <div>
-                 <CardItem src='images/img-9.jpg' text='Use my recipe finder to create your next great meal'
+                 <ProblemCardItem src='images/img-9.jpg' text='Use my recipe finder to create your next great meal'
                 label='Cook' />
-                <button className='btn btn--primary' onClick={this.handleClick}>Click here for more</button>
+               
               </div>
               <div className='problem_card'>
                 <p className='problem_text'>Problem: How to train a dog</p>
-                <button className='btn  btn--primary'  onClick={this.handleClick}>Click here for more</button>
+               
+              </div>
+            </ReactCardFlip>
+
+<ReactCardFlip isFlipped={this.state.isFlipped}flipSpeedBackToFront={2}
+              flipSpeedFrontToBack={2} >
+              <div>
+                 <ProblemCardItem src='images/img-9.jpg' text='Use my recipe finder to create your next great meal'
+                label='Cook' />
+               
+              </div>
+              <div className='problem_card'>
+                <p className='problem_text'>Problem: How to train a dog</p>
+                
               </div>
             </ReactCardFlip>
 
@@ -128,4 +135,4 @@ constructor(props) {
     );
   }
 }
-export default ProblemCardItem
+export default ProblemSolving
